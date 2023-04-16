f1379b5bf96881584886c398c7555e8104b78a90
TomCroot <thomascroot@hotmail.co.uk>
2023-04-16 18:19:59 +0100

    added working login

--- users/views.py
+++ users/views.py
@@ -2,9 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers.common import UserSerializer
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 import jwt
- 
+from datetime import datetime, timedelta
+from django.conf import settings
 from lib.exceptions import exceptions
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class RegisterView(APIView):
     @exceptions
@@ -13,3 +17,22 @@ class RegisterView(APIView):
         user_to_add.is_valid(raise_exception=True)
         user_to_add.save()
         return Response(user_to_add.data, status.HTTP_201_CREATED)
+    
+class LoginView(APIView):
+    @exceptions
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user_to_login = User.objects.get(email=email)
+        if not user_to_login.check_password(password):
+            print('PASSWORDS DONT MATCH')
+            raise PermissionDenied('Unauthorized')
+    
+        dt = datetime.now() + timedelta(days=7)
+
+
+        token = jwt.encode({ 'sub':  user_to_login.id, 'exp': int(dt.strftime('%S')) }, settings.SECRET_KEY, algorithm='HS256')
+        print('TOKEN ->', token)
+        
+        return Response({ 'message': f"Welcome back, {user_to_login.username}", 'token': token })
+    
