ad561bc0e51c4840e7bc10c340c3c9c710d04122
TomCroot <thomascroot@hotmail.co.uk>
2023-05-27 17:24:42 +0100

    mobile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,15 +5,15 @@ const Home = () => {
 
   const [backgroundIndex, setBackgroundIndex] = useState(0)
   const backgrounds = [
+    'https://d3qfk7u7s63iel.cloudfront.net/media/zoo/images/Fans_celebrating_the_success_of_their_favorite_sports_team_in_stadium_9ac92bcf1110fc5b1c18b5c8a0bd25df.jpg',
     'https://static7.depositphotos.com/1000423/702/i/450/depositphotos_7020377-stock-photo-crowd-on-the-stadium.jpg',
-    'https://st2.depositphotos.com/1518767/11356/i/450/depositphotos_113560446-stock-photo-silhouettes-of-football-supporters.jpg', 
-    'https://d3qfk7u7s63iel.cloudfront.net/media/zoo/images/Fans_celebrating_the_success_of_their_favorite_sports_team_in_stadium_9ac92bcf1110fc5b1c18b5c8a0bd25df.jpg'
+    'https://st2.depositphotos.com/1518767/11356/i/450/depositphotos_113560446-stock-photo-silhouettes-of-football-supporters.jpg'
   ]
 
   useEffect(() => {
     const interval = setInterval(() => {
       setBackgroundIndex((prevIndex) => (prevIndex + 1) % backgrounds.length)
-    }, 5000)
+    }, 2000)
 
     return () => {
       clearInterval(interval)
